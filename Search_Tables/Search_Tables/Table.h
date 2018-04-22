01b07d95239da9e4c375d53e7dc54d644cf5fc2e
owerbat <niktimak@gmail.com>
2018-04-22 15:33:31 +0300

    TTable was finished

--- Search_Tables/Search_Tables/Table.h
+++ Search_Tables/Search_Tables/Table.h
@@ -12,5 +12,31 @@ public:
 
 template <class TKey, class TValue>
 class TTable {
+protected:
+	int dataCount;
+	int efficiency;
+public:
+	TTable() { dataCount = 0; efficiency = 0; }
 
+	bool IsEmpty() { return dataCount == 0; }
+	virtual bool IsFull() = 0;
+
+	virtual bool Find(TKey fkey) = 0;
+	virtual void Insert(TRecord<TKey, TValue> rec) = 0;
+	virtual void Delete(TKey dkey) = 0;
+
+	virtual void Reset() = 0;
+	virtual bool IsEnd() = 0;
+	virtual void GoNext() = 0;
+
+	virtual TRecord<TKey, TValue> GetCurrent() = 0;
+	virtual void SetCurrValue(TValue val) = 0;
+
+	void Print() {
+		TRecord<TKey, TValue> tmp;
+		for (Reset(); !IsEnd(); GoNext()) {
+			tmp = GetCurrent();
+			cout << tmp.value << endl;
+		}
+	}
 };
