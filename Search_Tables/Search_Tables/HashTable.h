1c8362d65c0a6bd0997c2013668c3642993d501c
owerbat <niktimak@gmail.com>
2018-05-12 21:54:56 +0300

    Hash tables was added

--- Search_Tables/Search_Tables/HashTable.h
+++ Search_Tables/Search_Tables/HashTable.h
@@ -22,7 +22,7 @@ protected:
 	TRecord<TKey, TValue> *arr;
 public:
 	TArrayHash(int _size = 100);
-	TArrayHash();
+	~TArrayHash();
 
 	bool IsFull() { return dataCount == size; }
 
@@ -36,4 +36,147 @@ public:
 
 	TRecord<TKey, TValue> GetCurrent();
 	void SetCurrValue(TValue val);
+
+	void Print();
+	void PrintAll();
+
+	friend char *GetString(string str);
 };
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------
+
+
+
+template <class TKey, class TValue>
+TArrayHash<TKey, TValue>::TArrayHash(int _size) {
+	size = _size;
+	step = 13;
+	arr = new TRecord<TKey, TValue>[size];
+	for (int i = 0; i < size; i++) {
+		arr[i].key = "";
+	}
+}
+
+template <class TKey, class TValue>
+TArrayHash<TKey, TValue>::~TArrayHash() {
+	delete[] arr;
+}
+
+template <class TKey, class TValue>
+bool TArrayHash<TKey, TValue>::Find(TKey fkey) {
+	currNum = HashFunc(fkey) % size;
+	int freepos = -1;
+
+	for (int i = 0; i < size; i++) {
+		efficiency++;
+
+		if (arr[currNum].key == "") {
+			if (freepos == -1) {
+				return false;
+			}
+			else {
+				currNum = freepos;
+				return false;
+			}
+		}
+
+		if (arr[currNum].key == fkey) {
+			return true;
+		}
+
+		if ((freepos == -1) && (arr[currNum].key == "&")) {
+			freepos = currNum;
+		}
+
+		currNum = (currNum + step) % size;
+	}
+	return false;
+}
+
+template <class TKey, class TValue>
+void TArrayHash<TKey, TValue>::Insert(TRecord<TKey, TValue> rec) {
+	if (!Find(rec.key)) {
+		arr[currNum] = rec;
+		dataCount++;
+	}
+}
+
+template <class TKey, class TValue>
+void TArrayHash<TKey, TValue>::Delete(TKey dkey) {
+	if (Find(dkey)) {
+		arr[currNum].key = "&";
+		dataCount--;
+	}
+}
+
+template <class TKey, class TValue>
+void TArrayHash<TKey, TValue>::Reset() {
+	currNum = 0;
+	while (((arr[currNum].key == "&") || (arr[currNum].key == "")) && (currNum < size)) {
+		currNum++;
+	}
+}
+
+template <class TKey, class TValue>
+bool TArrayHash<TKey, TValue>::IsEnd() {
+	return currNum >= size;
+}
+
+template <class TKey, class TValue>
+void TArrayHash<TKey, TValue>::GoNext() {
+	while ((++currNum < size)) {
+		if (((arr[currNum].key != "&") && (arr[currNum].key != "")))
+			break;
+		//currNum++;
+	}
+}
+
+template <class TKey, class TValue>
+TRecord<TKey, TValue> TArrayHash<TKey, TValue>::GetCurrent() {
+	if (currNum < 0 || currNum >= size)
+		throw "Current number is incorrect";
+	return arr[currNum];
+}
+
+template <class TKey, class TValue>
+void TArrayHash<TKey, TValue>::SetCurrValue(TValue val) {
+	arr[currNum].value = val;
+}
+
+template <class TKey, class TValue>
+void TArrayHash<TKey, TValue>::Print() {
+	TRecord<TKey, TValue> tmp;
+	int i = 0;
+	for (Reset(); !IsEnd(); GoNext()) {
+		tmp = GetCurrent();
+		cout << currNum << ") " << GetString(tmp.key) << " - " << tmp.value << endl;
+		//if(currNum < size - 1)
+		//currNum++;
+	}
+}
+
+template <class TKey, class TValue>
+void TArrayHash<TKey, TValue>::PrintAll() {
+	TRecord<TKey, TValue> tmp;
+	for (int i = 0; i < size; i++) {
+		currNum = i;
+		tmp = GetCurrent();
+		cout << currNum << ") " << GetString(tmp.key) << " - " << tmp.value << endl;
+		//if(currNum < size - 1)
+		//currNum++;
+	}
+}
+
+char *GetString(string str) {
+	int len = str.length();
+	char *res = new char[len + 1];
+	for (int i = 0; i < len; i++) {
+		res[i] = str[i];
+	}
+	res[len] = '\0';
+	return res;
+}
--- Search_Tables/Search_Tables/main.cpp
+++ Search_Tables/Search_Tables/main.cpp
@@ -3,26 +3,74 @@
 #include "TreeTable.h"
 #include <time.h>
 
+string RandomSrting(int size);
+
 int main() {
 	//srand(time(NULL));
-	int m = 10, n = m + 10;
+	/*int m = 10, n = m + 10;
 	TScanTable<int, int> t(n);
 	TSortTable<int, int> s(n);
+	int *keys1 = new int[n], *keys2 = new int[n];
 
 	for (int i = 0; i < m; i++) {
 		TRecord<int, int> tmp1(rand() % 1000, rand() % 1000), tmp2(rand() % 1000, rand() % 1000);
+		keys1[i] = tmp1.key;
+		keys2[i] = tmp2.key;
 		if(!t.IsFull())
 			t.Insert(tmp1);
 		if(!s.IsFull())
 			s.Insert(tmp2);
 	}
 
-	s = t;
+	//s = t;
 
 	cout << "t:\n";
 	t.Print();
 	cout << "\ns:\n";
 	s.Print();
+
+	t.Delete(keys1[9]);
+	s.Delete(keys2[4]);
+
+	cout << "t:\n";
+	t.Print();
+	cout << "\ns:\n";
+	s.Print();*/
+	
+
+	int n = 20;
+	TArrayHash<string, int> tab(n);
+	string *keys = new string[n];
+
+	for (int i = 0; i < n/2; i++) {
+		TRecord<string, int> tmp(RandomSrting(5), rand() % 1000);
+		keys[i] = tmp.key;
+		if (!tab.IsFull()) {
+			tab.Insert(tmp);
+		}
+	}
+
+	cout << "tab:\n";
+	tab.Print();
+	cout << "\nall tab:\n";
+	tab.PrintAll();
+
+	tab.Delete(keys[0]);
+
+	cout << "tab:\n";
+	tab.Print();
+	cout << "\nall tab:\n";
+	tab.PrintAll();
 	
 	return 0;
+}
+
+
+
+string RandomSrting(int size) {
+	string res = "";
+	for (int i = 0; i < size; i++) {
+		res += (char)(rand() % 26 + 'a');
+	}
+	return res;
 }
