34e2ddd4bd77984733b345e8b7aa97759beed97b
owerbat <niktimak@gmail.com>
2018-05-15 20:49:52 +0300

    Tree tables was started

--- Search_Tables/Search_Tables/TreeTable.h
+++ Search_Tables/Search_Tables/TreeTable.h
@@ -1,7 +1,29 @@
 #pragma once
 #include "Table.h"
+#include <stack>
+
+template <class TKey, class TValue>
+class TNode {
+	TRecord<TKey, TValue> record;
+	TNode *pLeft, pRight;
+	int balance;
+};
 
 template <class TKey, class TValue>
 class TTreeTable : public TTable<TKey, TValue> {
+protected:
+	TNode *pRoot, *pCurr;
+	TNode **pRes;
+	stack<TNode *> st;
+public:
+	virtual bool Find(TKey fkey);
+	virtual void Insert(TRecord<TKey, TValue> rec);
+	virtual void Delete(TKey dkey);
 
+	virtual void Reset();
+	virtual bool IsEnd();
+	virtual void GoNext();
+
+	virtual TRecord<TKey, TValue> GetCurrent() { return pCurr->record; }
+	virtual void SetCurrValue(TValue val) { pCurr->record.value = val; }
 };
